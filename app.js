4e078f1391936b5c2a815dea960f9e7d7638e89b
Chinemere chukwu <chukwuchinemere@gmail.com>
2020-12-11 12:50:59 +0100

    fixed the iregularities in the time

--- app.js
+++ app.js
@@ -1,34 +1,74 @@
 
+// window.onload = function digitalTime() {
+//     var date = new Date();
+//     var hour = date.getHours();
+//     var minute = date.getMinutes();
+//     var zero1 = [1];
+//     // document.getElementById('figuremain').innerHTML = "" + hour + ":" + minute;
+//     if (minute <= 9 & hour <= 9) {
+//         document.getElementById('figuremain').innerHTML = "" + "0" + hour + ":" + "0" + minute;
+
+//     } else if (minute > 9 & hour > 9) {
+//         // document.getElementById('figuremain').innerHTML = "" + hour + ":" + "0" + minute;
+//         document.getElementById('figuremain').innerHTML = "" + hour + ":" + minute;
+
+
+//     } else if (minute <= 9 || hour <= 9) {
+//         document.getElementById('figuremain').innerHTML = "" + "0" + hour + ":" + "0" + minute;
+
+//     }
+
+//     if (hour <= 9) {
+//         document.getElementById('figuremain').innerHTML = "" + "0" + hour + ":" + minute;
+
+//     } else {
+//         document.getElementById('figuremain').innerHTML = "" + hour + ":" + minute;
+
+//     }
+
+// }
+
+// setInterval(window.onload, 60, 000)
+
+
 window.onload = function digitalTime() {
     var date = new Date();
     var hour = date.getHours();
     var minute = date.getMinutes();
     var zero1 = [1];
     // document.getElementById('figuremain').innerHTML = "" + hour + ":" + minute;
-    if (minute <= 9 & hour <= 9) {
+    if (hour <= 9 & minute <= 9) {
         document.getElementById('figuremain').innerHTML = "" + "0" + hour + ":" + "0" + minute;
 
-    } else if (minute > 9 & hour > 9) {
-        // document.getElementById('figuremain').innerHTML = "" + hour + ":" + "0" + minute;
+    } else if (hour > 9 & minute > 9) {
+        // Dont touch the code;
         document.getElementById('figuremain').innerHTML = "" + hour + ":" + minute;
-
-
-    } else if (minute <= 9 || hour <= 9) {
-        document.getElementById('figuremain').innerHTML = "" + "0" + hour + ":" + "0" + minute;
-
     }
+    //  else if (hour <= 9 || minute <= 9) {
+    //     document.getElementById('figuremain').innerHTML = "" + "0" + hour + ":" + "0" + minute;
 
-    if (hour <= 9) {
+    // }
+
+    if (hour <= 9 & minute > 9) {
         document.getElementById('figuremain').innerHTML = "" + "0" + hour + ":" + minute;
 
-    } else {
-        document.getElementById('figuremain').innerHTML = "" + hour + ":" + minute;
+    }
+    else if (hour > 9 & minute <= 9) {
+        document.getElementById('figuremain').innerHTML = "" + hour + ":" + "0" + minute;
 
     }
 
 }
 
 setInterval(window.onload, 60, 000)
+
+
+
+
+
+
+
+
 function days() {
     var monthOfTheYear = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"]
     var daysOfTheWeek = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESSDAY", "THURSDAY", "FRIDAY", "SATURDAY"]
@@ -50,8 +90,6 @@ function days() {
 
 }
 days()
-
-
 
 
 
