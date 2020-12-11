2c1896db3707e6919957fc29b116807c22b967c7
Chinemere chukwu <chukwuchinemere@gmail.com>
2020-12-11 15:40:48 +0100

    added comments where neccesary

--- app.js
+++ app.js
@@ -1,53 +1,16 @@
 
-// window.onload = function digitalTime() {
-//     var date = new Date();
-//     var hour = date.getHours();
-//     var minute = date.getMinutes();
-//     var zero1 = [1];
-//     // document.getElementById('figuremain').innerHTML = "" + hour + ":" + minute;
-//     if (minute <= 9 & hour <= 9) {
-//         document.getElementById('figuremain').innerHTML = "" + "0" + hour + ":" + "0" + minute;
-
-//     } else if (minute > 9 & hour > 9) {
-//         // document.getElementById('figuremain').innerHTML = "" + hour + ":" + "0" + minute;
-//         document.getElementById('figuremain').innerHTML = "" + hour + ":" + minute;
-
-
-//     } else if (minute <= 9 || hour <= 9) {
-//         document.getElementById('figuremain').innerHTML = "" + "0" + hour + ":" + "0" + minute;
-
-//     }
-
-//     if (hour <= 9) {
-//         document.getElementById('figuremain').innerHTML = "" + "0" + hour + ":" + minute;
-
-//     } else {
-//         document.getElementById('figuremain').innerHTML = "" + hour + ":" + minute;
-
-//     }
-
-// }
-
-// setInterval(window.onload, 60, 000)
-
-
+// Function that displays the time on the screem
 window.onload = function digitalTime() {
     var date = new Date();
     var hour = date.getHours();
     var minute = date.getMinutes();
-    var zero1 = [1];
-    // document.getElementById('figuremain').innerHTML = "" + hour + ":" + minute;
+
     if (hour <= 9 & minute <= 9) {
         document.getElementById('figuremain').innerHTML = "" + "0" + hour + ":" + "0" + minute;
 
     } else if (hour > 9 & minute > 9) {
-        // Dont touch the code;
         document.getElementById('figuremain').innerHTML = "" + hour + ":" + minute;
     }
-    //  else if (hour <= 9 || minute <= 9) {
-    //     document.getElementById('figuremain').innerHTML = "" + "0" + hour + ":" + "0" + minute;
-
-    // }
 
     if (hour <= 9 & minute > 9) {
         document.getElementById('figuremain').innerHTML = "" + "0" + hour + ":" + minute;
@@ -59,16 +22,9 @@ window.onload = function digitalTime() {
     }
 
 }
-
 setInterval(window.onload, 60, 000)
 
-
-
-
-
-
-
-
+// function that writes the today's date day and month on the scrollig marquess
 function days() {
     var monthOfTheYear = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"]
     var daysOfTheWeek = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESSDAY", "THURSDAY", "FRIDAY", "SATURDAY"]
@@ -79,15 +35,15 @@ function days() {
     var todayDate = date.getDate();
     var month = date.getMonth();
     var year = date.getFullYear();
+    // for (var i = 0; i < daysOfTheWeek.length; i++) {
+    //     if (i === day) {
+    //         document.getElementById("today").innerHTML = "TODAY IS" + " " + daysOfTheWeek[i] + "," + " " + monthOfTheYear[month] + " " + todayDate + "," + " " + year + ".";
 
-    for (var i = 0; i < daysOfTheWeek.length; i++) {
-        if (i === day) {
-            document.getElementById("today").innerHTML = "TODAY IS" + " " + daysOfTheWeek[i] + "," + " " + monthOfTheYear[month] + " " + todayDate + "," + " " + year + ".";
+    //     }
 
-        }
+    // }
 
-    }
-
+    document.getElementById("today").innerHTML = "TODAY IS" + " " + daysOfTheWeek[day] + "," + " " + monthOfTheYear[month] + " " + todayDate + "," + " " + year + ".";
 }
 days()
 
